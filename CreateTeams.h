94332add3f32b16a66e9cc44a39c7061783d27b0
AFaltin <84854310+AFaltin@users.noreply.github.com>
2021-06-16 23:42:27 +0300

    Delete CreateTeams.h

--- CreateTeams.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include <string>
-#include <list>
-#include "Player.h"
-#include "Scheme.h"
-#include "Trainer.h"
-#include "Team.h"
-#include "CreateTeam.h"
-void CreateTeams(std::list <Player> players, Trainer trainer1, Trainer trainer2, Trainer trainer3, Scheme scheme1, Scheme scheme2, Scheme scheme3);
