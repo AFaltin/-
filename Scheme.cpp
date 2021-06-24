688564760b3b1865c97cd07603ad377d926abddc
AFaltin <84854310+AFaltin@users.noreply.github.com>
2021-06-24 15:18:35 +0300

    Update Scheme.cpp

--- Scheme.cpp
+++ Scheme.cpp
@@ -3,10 +3,10 @@
 
 Scheme ::Scheme(int BackQuantity1, int HalfBackQuantity1, int ForwardQuantity1)
 {
-   /* if (BackQuantity < 0 || HalfBackQuantity < 0 || ForwardQuantity < 0 || BackQuantity + HalfBackQuantity + ForwardQuantity>11)
+    if (BackQuantity < 0 || HalfBackQuantity < 0 || ForwardQuantity < 0 || BackQuantity + HalfBackQuantity + ForwardQuantity>11)
     {
         throw std::invalid_argument("Wrong scheme");
-    }*/
+    }
     BackQuantity = BackQuantity1;
     HalfBackQuantity = HalfBackQuantity1;
     ForwardQuantity = ForwardQuantity1;
